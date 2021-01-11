6e7a9d28d5ff9ee0fb2b2b042c5c4031d361c111
ghofranemessaoudi <messaoudighofrane2@gmail.com>
2021-01-11 15:56:45 +0100

    add midelwear

--- api/index.js
+++ api/index.js
@@ -154,8 +154,16 @@ routers.use("/activity", activity);
 const chats = require("./routes/chats.js");
 routers.use("/chats", chats);
 
+const checkAuthenticated = (req, res,next) => {
+if(req.isAuthenticated()){
+  return next()
+}else{
+  return { username: false }
+}
+}
+
 var users = require("./routes/users.js");
-routers.use("/users", users);
+routers.use("/users",checkAuthenticated, users);
 // View engine setup
 
 app.use("/api", routers);
